2dfe537e19b0f69be4656d2e01d49144943b6266
nknatalie <natkarpowicz392@gmail.com>
2024-03-13 22:50:28 +0100

    Update Projket.py

--- Projket.py
+++ Projket.py
@@ -2,13 +2,16 @@ from tkinter import *
 from PIL import *
 
 # set colours
-bg_colour = "#3d6466"
+bg_colour = 'white' #"#3d6466"
 
 
 def load_frame1():
-	frame1.tkraise()
-	wybierz_gre= Button(frame1, text="cos")
+	page1_open.tkraise()
+	page1_open.rowconfigure(0,weight=1)
+	page1_open.columnconfigure(0,weight=1)
+	wybierz_gre= Button(page1_open, text="cos")
 	wybierz_gre.grid(row=0, column=0)
+
 
 # initiallize app with basic settings
 root = Tk()
@@ -17,13 +20,19 @@ root.title("Recipe Picker")
 x = root.winfo_screenwidth() // 2
 y = int(root.winfo_screenheight() * 0.1)
 root.geometry('500x600+' + str(x) + '+' + str(y))
- 
+root.resizable(1,1)
+root.option_add('*Font', 'Arial 12')
+root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
+root.tk_setPalette(background='white')
+
+
 # create a frame widgets
-frame1 = Frame(root, width=500, height=600, bg=bg_colour)
-frame2 = Frame(root, bg=bg_colour)
+page1_open = Frame(root, width=500, height=600, bg=bg_colour)
+page2_ = Frame(root, bg=bg_colour)
 
 # place frame widgets in window
-for frame in (frame1, frame2):
+for frame in (page1_open, page2_):
 	frame.grid(row=0, column=0, sticky="nesw")
 
 # load the first frame
