cb7f63601edc2328bdf2825cb8b415c0054108a8
nknatalie <natkarpowicz392@gmail.com>
2024-03-19 20:44:43 +0100

    Update Projket.py

--- Projket.py
+++ Projket.py
@@ -2,27 +2,27 @@ from tkinter import *
 from PIL import *
 from customtkinter import *
 # set colours
-bg_colour = 'navy' #"#3d6466"
-
+bg_colour = '#3652AD'
 # the first frame 
 def load_frame1():
 	page1_start.tkraise()
 	page1_start.rowconfigure((0,1,2,3,4,5,6,7,8),weight=1)
 	page1_start.columnconfigure((0,1,2,3,4,5),weight=1)
-	ramka=Frame(page1_start, bg='blue', width=1600, height=800)
+	ramka=CTkFrame(page1_start, width=1600, height=1000,corner_radius=10,fg_color='#FFEAA7')
+	#ramka.place(relx=0.5,rely=0.5, anchor=CENTER)
 	#ramka.place(x=150,y=100)
-	ramka.grid(row=1, rowspan=6, column=1, columnspan=4)
-	info_wr= Label(page1_start,text="Wybierz rozgrywkę",fg='black',bg='white', font=('Arial',26) )
+	#ramka.grid(row=1, rowspan=6, column=1, columnspan=4)
+	ramka.grid(row=1, rowspan=7, column=1, columnspan=4, sticky="nsew")
+	info_wr= CTkLabel(ramka,text="Wybierz rozgrywkę",fg_color='white', font=('Arial',26),corner_radius=32 )
 	info_wr.grid(row=2,column=2,columnspan=2,ipadx=100, ipady=20)
-	style = {"fg": "white", "bg": bg_colour, "font": ("Arial", 20), "bd": 2, "relief": "raised"}
 
-	wybor_cr= CTkButton(page1_start, text="Czas reakcji",command=load_frame2,**style, corner_radius=32) # czy chcemy zaokrąglone rogi?
+	wybor_cr= CTkButton(ramka, text="Czas reakcji",  command=load_frame2, font=('Arial',20),corner_radius=32) 
 	wybor_cr.grid(row=3, column=1,columnspan=2,ipadx=170, ipady=40)
-	wybor_tr=Button(page1_start,text="Trening pamięci",command=load_frame3,**style)
+	wybor_tr=CTkButton(ramka,text="Trening pamięci",command=load_frame3,font=('Arial',20),corner_radius=32)
 	wybor_tr.grid(row=3,column=3,columnspan=2,ipadx=170, ipady=40)
-	wybor_ka= Button(page1_start, text="Kolejność matematyczna",command=load_frame3, **style )
+	wybor_ka= CTkButton(ramka, text="Kolejność matematyczna",command=load_frame3, font=('Arial',20),corner_radius=32)
 	wybor_ka.grid(row=5, column=1,columnspan=2,ipadx=100, ipady=40)
-	wybor_rm= Button(page1_start, text="Równanie matemtyczne",command=load_frame3, ** style )
+	wybor_rm= CTkButton(ramka, text="Równanie matemtyczne",command=load_frame3, font=('Arial',20),corner_radius=32)
 	wybor_rm.grid(row=5, column=3,columnspan=2,ipadx=120, ipady=40)
 
 
