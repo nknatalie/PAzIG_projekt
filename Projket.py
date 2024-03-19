8e6aa4644ce12c724e9a782a05734628345bc6ec
nknatalie <natkarpowicz392@gmail.com>
2024-03-19 19:32:42 +0100

    Update Projket.py

--- Projket.py
+++ Projket.py
@@ -13,17 +13,17 @@ def load_frame1():
 	ramka=Frame(page1_start, bg='blue', width=1600, height=800)
 	#ramka.place(x=150,y=100)
 	ramka.grid(row=1, rowspan=6, column=1, columnspan=4)
-	info_wr= Label(page1_start,text="Wybierz rozgrywkę",fg='black',bg='white')
-	info_wr.grid(row=2,column=2,columnspan=2,padx=20,pady=40, sticky='ew')
+	info_wr= Label(page1_start,text="Wybierz rozgrywkę",fg='black',bg='white', font=('Arial',26) )
+	info_wr.grid(row=2,column=2,columnspan=2,ipadx=100, ipady=20)
 
 	wybor_cr= Button(page1_start, text="Czas reakcji",command=load_frame2,fg='white',bg='navy') # czy chcemy zaokrąglone rogi?
-	wybor_cr.grid(row=3, column=1,columnspan=2,ipadx=100, ipady=40)
+	wybor_cr.grid(row=3, column=1,columnspan=2,ipadx=170, ipady=40)
 	wybor_tr=Button(page1_start,text="Trening pamięci",command=load_frame3,fg='white',bg='navy')
-	wybor_tr.grid(row=3,column=4,padx=20,pady=40, sticky='ew')
+	wybor_tr.grid(row=3,column=3,columnspan=2,ipadx=170, ipady=40)
 	wybor_ka= Button(page1_start, text="Kolejność matematyczna",command=load_frame3, fg='white',bg='navy' )
-	wybor_ka.grid(row=5, column=1,padx=20,pady=40, sticky='ew')
+	wybor_ka.grid(row=5, column=1,columnspan=2,ipadx=100, ipady=40)
 	wybor_rm= Button(page1_start, text="Równanie matemtyczne",command=load_frame3, fg='white',bg='navy' )
-	wybor_rm.grid(row=5, column=4,padx=20,pady=40, sticky='ew')
+	wybor_rm.grid(row=5, column=3,columnspan=2,ipadx=120, ipady=40)
 
 
 def load_frame2():
