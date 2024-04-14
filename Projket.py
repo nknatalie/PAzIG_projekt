68590776a2d3b0aad623aa5b8aaec5bbf4733fa2
Niddhoggr <bs302651@student.polsl.pl>
2024-04-14 22:30:55 +0200

    Implementacja logiki dla ćwiczenia: "Trening pamięci"

--- Projket.py
+++ Projket.py
@@ -87,14 +87,98 @@ def load_frame3(poziom_trudnosci):
 
 def load_frame4(poziom_trudnosci):
 	page4_TreningPamieci.tkraise()
-	info_zapamietajKolejnosc=CTkLabel(page4_TreningPamieci,text='Zapamiętaj kolejność!',fg_color='white', font=('Arial',60,'bold'),corner_radius=32,width=250, height=75 ) 
-	info_zapamietajKolejnosc.place(relx=0.5,rely=0.05,anchor='n')
-	if poziom_trudnosci==1:
-		pass
-	elif poziom_trudnosci==2:
-		pass
-	elif poziom_trudnosci==3:
-		pass
+	liczba_rund=4 if poziom_trudnosci==1 else  6 if poziom_trudnosci==2 else 9 
+	liczba_obrazkow =3 if poziom_trudnosci==1 else 4 if poziom_trudnosci==2 else 5
+	obrazki = [CTkLabel(page4_TreningPamieci,text='') for _ in range(liczba_obrazkow)]
+	comboboxy=[CTkComboBox(page4_TreningPamieci,values=[str(i + 1) for i in range(liczba_obrazkow)])for _ in range (liczba_obrazkow)]
+	
+	info_ZapamietajPodajKolejnosc=CTkLabel(page4_TreningPamieci,text='Zapamiętaj kolejność!',fg_color='white', font=('Arial',60,'bold'),corner_radius=32,width=250, height=75 ) 
+	info_ZapamietajPodajKolejnosc.place(relx=0.5,rely=0.05,anchor='n')
+	mierzy_czas=CTkLabel(page4_TreningPamieci,text='',fg_color='white', font=('Arial',60,'bold'),corner_radius=32,width=250, height=75)
+	mierzy_czas.place(relx=0.5,rely=0.90, anchor='s')
+	button_dalej=CTkButton(page4_TreningPamieci,text='Dalej', font=('Arial',60,'bold'),corner_radius=32,width=250, height=75)
+
+	
+	def nowarunda():
+		nonlocal liczba_rund
+		liczba_rund -=1
+		if 0<=liczba_rund:
+			for obrazek in obrazki:
+				obrazek.configure(image='')
+				obrazek.place_forget()
+			for combobox in comboboxy:
+				combobox.place_forget()
+			button_dalej.place_forget()
+			mierzy_czas.place(relx=0.5,rely=0.90, anchor='s')
+			start_rundy()
+
+	def sprawdz(orginalna,etykieta,combo): 
+		tabZgodnisci=[]
+		for i in range(len(etykieta)):
+			sprawdzana=combo[i].get()
+			#print(sprawdzana,type(sprawdzana))
+			if sprawdzana:
+				if orginalna[i]==etykieta[int(sprawdzana)-1]:
+					tabZgodnisci.append(1)
+				else:
+					tabZgodnisci.append(0)
+			else:
+				break
+		nowarunda()
+
+		return tabZgodnisci
+	
+	def start_rundy():
+		nonlocal liczba_rund
+		if 0<=liczba_rund:
+			tablica=[] 
+			while len(tablica) < liczba_obrazkow:
+				x = random.randrange(1, 8)
+				if x not in tablica:
+					tablica.append(x)
+					obrazek = Image.open(f"Treningpamieci\{x}.png")
+					obrazek = obrazek.resize((300, 300), Image.BILINEAR)
+					obrazek = ImageTk.PhotoImage(obrazek)
+					obrazki[len(tablica) - 1].configure(image=obrazek)  
+					obrazki[len(tablica) - 1].place(relx=len(tablica)  / (liczba_obrazkow + 1), rely=0.40, anchor='center')
+					#print(tablica[len(tablica) - 1]) 
+
+			start_time=time.perf_counter()
+			while time.perf_counter()-start_time<5:
+				pozostaly_czas=int(5-(time.perf_counter()-start_time))
+				mierzy_czas.configure(text=f'{pozostaly_czas}')
+				page4_TreningPamieci.update()
+			mierzy_czas.place_forget() 
+					
+			tablica2=tablica.copy()
+
+			random.shuffle(tablica)
+					#print(tablica)
+					
+			for i,x in enumerate(tablica):
+				obrazek = Image.open(f"Treningpamieci\{x}.png")
+				obrazek = obrazek.resize((300, 300), Image.BILINEAR)
+				obrazek = ImageTk.PhotoImage(obrazek)
+				obrazki[i].configure(image=obrazek)  
+				obrazki[i].place(relx=(i + 1) / (liczba_obrazkow + 1), rely=0.40, anchor='center')
+				#print(tablica[i])
+				combobox = comboboxy[i]
+				combobox.place(relx=(i + 1) / (liczba_obrazkow + 1), rely=0.60, anchor='center')
+				combobox.set('')
+
+			info_ZapamietajPodajKolejnosc.configure(text='Podaj kolejność!')
+			page4_TreningPamieci.update()
+		
+		if 0<liczba_rund:
+			button_dalej.place(relx=0.5,rely=0.90, anchor='s')
+			button_dalej.configure(text='Dalej')
+			button_dalej.configure(command= lambda: sprawdz(tablica2,tablica,comboboxy))
+		else:
+			button_dalej.place(relx=0.5,rely=0.90, anchor='s')
+			button_dalej.configure(text='Koniec')
+			button_dalej.configure(command=lambda: koniec ("równania matematycznego", poziom_trudnosci))
+
+	start_rundy()
 
 def load_frame5(poziom_trudnosci):
 	page5_KolejnoscAlfabetyczna.tkraise()
